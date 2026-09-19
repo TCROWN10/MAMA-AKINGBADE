dfb7de8cb8c372b1bfed576cb60ce7be87fda393
TCROWN10 <bolajioluwatobi249@gmail.com>
2026-09-19 16:28:43 +0100

    Update name placeholder to Akingbade only.

--- src/components/ReservationForm.tsx
+++ src/components/ReservationForm.tsx
@@ -259,7 +259,7 @@ export function ReservationForm({
             name="name"
             value={form.name}
             onChange={(e) => update("name", e.target.value)}
-            placeholder="e.g. Adeola Akingbade"
+            placeholder="e.g. Akingbade"
             autoComplete="name"
           />
         </Field>
